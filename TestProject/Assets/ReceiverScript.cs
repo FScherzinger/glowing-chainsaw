f6d9d452934abc043ad86e077c8fc52411d1da65
Thorsten <thorsten.schamper@gmail.com>
2016-08-17 16:18:30 +0200

    establishing connection in different threads to prevent the unity editor from freezing

--- TestProject/Assets/ReceiverScript.cs
+++ TestProject/Assets/ReceiverScript.cs
@@ -11,30 +11,40 @@ using de.dfki.test;
 
 public class ReceiverScript : MonoBehaviour {
 
-	static PSClient client;
-	Thread thread = new Thread(Receive);
+	static PSClient receive_client;
+	Thread receive_thread = new Thread( Receive );
 
-	void Start () {
-		Uri uri = PSFactory.CreateURI("receiver-client", "localhost", 9000);
-		client = PSFactory.CreatePSClient(uri);
-		client.Subscribe("TestEvent");
-		client.Connect();
-		thread.Start();
+	void Start()
+	{
+		receive_thread.Start();
 	}
 
-	void OnApplicationQuit(){
-		client.Disconnect();
-		thread.Abort();
+	void OnApplicationQuit()
+	{
+		receive_client.Disconnect();
+		receive_thread.Abort();
 	}
 
-	static void Receive () {
-		while(client.IsOnline())
-			while(client.CanRecv()){
-				de.dfki.tecs.Event eve = client.Recv();
-				if(eve.Is("TestEvent")){
+	static void Receive()
+	{
+		// establish connection
+		Debug.Log( "waiting for tecs-server... (receiver)" );
+		Uri uri = PSFactory.CreateURI( "receiver-client", "localhost", 9000 );
+		receive_client = PSFactory.CreatePSClient( uri );
+		receive_client.Subscribe( "TestEvent" );
+		receive_client.Connect();
+		Debug.Log( "connected. (receiver)" );
+
+		// start listening
+		while( receive_client.IsOnline() )
+			while( receive_client.CanRecv() )
+			{
+				de.dfki.tecs.Event eve = receive_client.Recv();
+				if( eve.Is( "TestEvent" ) )
+				{
 					TestEvent te = new TestEvent();
 					eve.ParseData(te);
-					Debug.Log(te.X + " , " + te.Y + " , " + te.Z);
+					Debug.Log( te.X + " , " + te.Y + " , " + te.Z );
 				}
 			}
 	}
