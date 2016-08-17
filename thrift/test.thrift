9e6f73ff9fb30c0693632b55ffacbadc024ae7af
Malte Groß <malte.gross@web.de>
2016-08-17 20:17:40 +0200

    thrift file showcase comments (java inline doc) & fixed & updated gitignore

--- thrift/test.thrift
+++ thrift/test.thrift
@@ -1,7 +1,13 @@
 namespace csharp de.dfki.test
-
+/**
+* TestEvent to test.
+**/
 struct TestEvent{
+
+        /** X-Coordinate **/
 	1:required double x;
+	/** Y-Coordinate **/
 	2:required double y;
+	/** Z-Coordinate **/
 	3:required double z;
 }
