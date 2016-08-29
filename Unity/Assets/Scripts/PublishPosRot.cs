83e25921182626553a9c3a2fd4ac474fa1737e4b
Malte Groß <s9mmgro2@stud.uni-saarland.de>
2016-08-29 18:22:13 +0200

    updated reciver & publisher scripts

--- Unity/Assets/Scripts/PublishPosRot.cs
+++ Unity/Assets/Scripts/PublishPosRot.cs
@@ -11,68 +11,90 @@ using de.dfki.events;
 
 public class PublishPosRot : MonoBehaviour {
 
-	static PSClient publish_client;
-	Thread connection_thread = new Thread( Connect );
+	Publisher publisher;
 	static bool is_connected = false;
+	public Device device;
 
 	void Start()
 	{
-		connection_thread.Start();
+		publisher = new Publisher ();
+		Thread publishThread = new Thread( publisher.Connect );
 	}
 
-	static void Connect()
-	{
-		Debug.Log( "waiting for tecs-server... (publisher)" );
-		Uri uri = PSFactory.CreateURI( "publish-client", "192.168.0.13", 9000 );
-		publish_client = PSFactory.CreatePSClient( uri );
-		publish_client.Connect();
-		is_connected = true;
-		Debug.Log( "connected. (publisher)" );
-	}
+
 
 	void OnApplicationQuit()
 	{
-		publish_client.Disconnect();
-		connection_thread.Abort();
+		publisher.close ();
 	}
 
 	void FixedUpdate(){
 
-		SendPosition();
-
+		publisher.SendPosition (gameObject);
+		publisher.SendRotation (gameObject);
 		
 	}
-
-	void Update()
-	{
-	}
-
-	void SendPosition()
-	{
-		if( !is_connected )
-			return;
-		if( publish_client.IsOnline() && publish_client.IsConnected() )
-		{
-			PositionEvent pe = new PositionEvent(getDevice(), new Position(transform.position),getID());
-			publish_client.Send( ".*", "PositionEvent", pe );
-		}
-	}
-	void SendRotation(){
-		if( !is_connected )
-			return;
-		if( publish_client.IsOnline() && publish_client.IsConnected() )
-		{
-			DirectionEvent de = new DirectionEvent( getDevice(), new Direction(transform.rotation),getID());
-			publish_client.Send( ".*", "DirectionEvent", de );
-		}
-	}
-
-	Device getDevice(){
-		return Device.GEARVR;
-	}
-
-	int getID(){
-		return 1;
-	}
 		
+
+
+
+
+	public class Publisher{
+		PSClient publish_client;
+		public Device device { get; set; }
+		public string serverAddr { get; set; }
+		public int serverPort { get; set;}
+		int id = 0;
+		public void Connect()
+		{
+			Debug.Log( "waiting for tecs-server... (publisher)" );
+			generateUniqueUri ();
+			Uri uri = PSFactory.CreateURI("publisher_"+device + "-" + id, serverAddr, serverPort );
+			publish_client = PSFactory.CreatePSClient( uri );
+			publish_client.Connect();
+			Debug.Log( "connected. (publisher)" );
+		}
+
+
+		void generateUniqueUri(){
+			Uri uri = PSFactory.CreateURI ("Discover", serverAddr, serverPort);
+			PSClient client = PSFactory.CreatePSClient(uri);
+			client.Connect ();
+			long ping = client.Ping ("publisher_"+device + "-" + id,2000);
+			if (ping>0){
+				id++;
+				generateUniqueUri();
+			}
+			Debug.Log ("Deveice:"+device.ToString()+" | id:"+id);
+		}
+
+
+
+		public void SendRotation(GameObject go){
+			if( !is_connected )
+				return;
+			
+			if( publish_client.IsOnline() && publish_client.IsConnected() )
+			{
+				DirectionEvent de = new DirectionEvent( device, new Direction(go.transform.rotation),id);
+				publish_client.Send( ".*", "DirectionEvent", de );
+			}
+		}
+
+		public void SendPosition(GameObject go)
+		{
+			if( !is_connected )
+				return;
+			if( publish_client.IsOnline() && publish_client.IsConnected() )
+			{
+				PositionEvent pe = new PositionEvent(device, new Position(go.transform.position),id);
+				publish_client.Send( ".*", "PositionEvent", pe );
+			}
+		}
+
+		public void close(){
+			publish_client.Disconnect();
+
+		}
+	}
 }
--- Unity/Assets/Scripts/ReceivePosRot.cs
+++ Unity/Assets/Scripts/ReceivePosRot.cs
@@ -9,21 +9,32 @@ using de.dfki.tecs.basetypes;
 using System.Runtime.InteropServices;
 
 using de.dfki.events;
+using System.Collections.Generic;
 
 public class ReceivePosRot : MonoBehaviour {
 
 	static PSClient receive_client;
-	public de.dfki.events.Device device;
 	Thread receive_thread;
 
-	static de.dfki.events.Direction direction;
-	static de.dfki.events.Position position;
-	static int id = 0;
-	public String s;
-	public int serverPort = 9000;
+
+	public GameObject Head;
+	public int id = 0;
+	public String serveradress = "localhost";
+	public int serverport = 9000;
+	public Device device;
+	public static volatile LinkedList<PositionEvent> PositionEvents;
+	public static volatile LinkedList<DirectionEvent> DirectionEvents;
+	public volatile Dictionary<int,GameObject> Heads;
+
 	void Start()
 	{
-		ReceiverThread rc_thread = new ReceiverThread ();
+
+		Heads = new Dictionary<int,GameObject>();
+		ReceiverThread rc_thread = new ReceiverThread{
+			serverAddr = this.serveradress,
+			serverPort = this.serverport,
+			device = this.device
+		};
 		receive_thread = new Thread (rc_thread.Connect);
 		receive_thread.Start();
 	}
@@ -41,47 +52,45 @@ public class ReceivePosRot : MonoBehaviour {
 	}
 
 	void updatePosition(){
-		if (position != null) {
-			Vector3 newpos = new Vector3((float)position.X, (float)position.Y, (float)position.Z);
-			transform.position = Vector3.Lerp (this.transform.position, newpos, Time.deltaTime * 5);
+		if (PositionEvents.Count == 0)
+			return;
+		PositionEvent pe = PositionEvents.First.Value;
+		DirectionEvents.RemoveFirst();
+		Vector3 newpos = new Vector3((float)pe.Position.X,(float)pe.Position.Y,(float)pe.Position.Z);
+
+		if (!Heads.ContainsKey (id)) {
+			GameObject head = Instantiate (Head);
+			Heads.Add(id,head);
 		}
+		Heads[id].transform.position =  Vector3.Lerp (this.transform.position, newpos, Time.deltaTime * 5);
 	}
 
+
+
 	void updateDirection(){
-		if (direction != null) {
-			Quaternion newrot = new Quaternion((float)direction.X, (float)direction.Y, (float)direction.Z, (float)direction.W);
-			Quaternion upatedrot = Quaternion.Lerp(this.transform.rotation, newrot, Time.deltaTime * 30);
-			Vector3 rot_vec = upatedrot.eulerAngles;
-			rot_vec.x = 270;
-			transform.rotation = Quaternion.Euler (rot_vec);
-		}
+//		if (direction != null) {
+//			Quaternion newrot = new Quaternion((float)direction.X, (float)direction.Y, (float)direction.Z, (float)direction.W);
+//			Quaternion upatedrot = Quaternion.Lerp(this.transform.rotation, newrot, Time.deltaTime * 30);
+//			Vector3 rot_vec = upatedrot.eulerAngles;
+//			rot_vec.x = 270;
+//			transform.rotation = Quaternion.Euler (rot_vec);
+//		}
 	}
 
 
 	public class ReceiverThread {
-		string serverAddr { get; set; }
-		int serverPort { get; set;}
-		Device device { get; set; }
+		public string serverAddr { get; set; }
+		public int serverPort { get; set;}
+		public Device device { get; set; }
 
 		public void Connect(){
 			Debug.Log( "waiting for tecs-server... (receiver)" );
-			Uri uri = PSFactory.CreateURI (device + "-" + id, serverAddr, serverPort);
+			Uri uri = PSFactory.CreateURI ("receiver_"+device, serverAddr, serverPort);
 			receive_client = PSFactory.CreatePSClient(uri);
 			receive_client.Subscribe( "DirectionEvent" );
 			receive_client.Subscribe( "PositionEvent" );
 			receive_client.Connect();
 			Debug.Log( "connected. (receiver)" );
-		}
-
-		void generateUniqueUri(){
-			Uri uri = PSFactory.CreateURI ("Discover", serverAddr, serverPort);
-			PSClient client = PSFactory.CreatePSClient(uri);
-			client.Connect ();
-			long ping = client.Ping (device + "-" + id,2000);
-			if (ping>0){
-				id++;
-				generateUniqueUri();
-			}
 		}
 
 		void Receive(Device device)
@@ -95,18 +104,12 @@ public class ReceivePosRot : MonoBehaviour {
 					if (eve.Is ("PositionEvent")) {
 						PositionEvent pos_event = new PositionEvent ();
 						eve.ParseData (pos_event);
-						if (pos_event.Id == id && pos_event.Type == device) {
-							position = pos_event.Position;
-						}
-						continue;
+						PositionEvents.AddLast(pos_event);
 					}
 					if (eve.Is ("RotationEvent")) {
 						DirectionEvent dir_event = new DirectionEvent ();	
 						eve.ParseData (dir_event);
-						if (dir_event.Id == id && dir_event.Type == device) {
-							direction = dir_event.Direction;
-						}
-						continue;
+						DirectionEvents.AddLast (dir_event);
 					}
 
 				}
