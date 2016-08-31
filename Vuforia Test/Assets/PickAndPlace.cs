0522479165d34eaf69ecdc077fb7ba32266d925d
Thorsten Schamper <thorsten.schamper@gmail.com>
2016-08-31 16:14:53 +0200

    implemented PickAndPlaceEvent + communicaiton with baxter

---
+++ Vuforia Test/Assets/PickAndPlace.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+//libtecs namespace
+using de.dfki.tecs.ps;
+
+//Baxter Namespace
+using de.dfki.tecs.robot.baxter;
+
+
+class PickAndPlace
+{
+
+    private PSClient client;
+
+    public string serverAddr { get; set; }
+    public int serverPort { get; set; }
+//    public MoveArmEvent move { get; set; }
+    public PickAndPlaceEvent pap { get; set; }
+
+
+    private void Connect()
+    {
+        Uri uri = PSFactory.CreateURI( "csharp-client-pap", serverAddr, serverPort );
+        client = PSFactory.CreatePSClient( uri );
+
+        // subscribe before connecting
+        client.Subscribe( "DoneEvent" );
+        client.Subscribe( "PickAndPlaceEvent" );
+        // connect
+        client.Connect();
+    }
+
+    public void Send( )
+    {
+        Connect();
+
+        if( client.IsOnline() && client.IsConnected() )
+        {
+            // Send event to client "receiver"
+            // client.Send([ClientAddress], [Event = Struct Name], [Struct])
+            client.Send( "baxter_dummy", "PickAndPlaceEvent", pap );
+            client.Disconnect();
+        }
+    }
+}
---
+++ Vuforia Test/Assets/SendEvent.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+using de.dfki.tecs.robot.baxter;
+
+public class SendEvent : MonoBehaviour {
+
+    PickAndPlace pap;
+    Thread thread;
+
+	// Use this for initialization
+	void Start () {
+
+        // create event
+        PickAndPlaceEvent pape = createPaPEvent();
+
+        pap = new PickAndPlace
+        {
+            serverAddr = "localhost",
+            serverPort = 9000,
+            pap = pape
+        };
+
+        thread = new Thread( pap.Send );
+        thread.Start();
+    }
+
+    private PickAndPlaceEvent createPaPEvent()
+    {
+        PickAndPlaceEvent pape = new PickAndPlaceEvent();
+        pape.Limb = Limb.LEFT;
+
+        pape.Initial_pos = new Position();
+        pape.Initial_pos.X_left = "1";
+        pape.Initial_pos.Y_left = "2";
+        pape.Initial_pos.Z_left = "3";
+
+        pape.Final_pos = new Position();
+        pape.Final_pos.X_left = "1";
+        pape.Final_pos.Y_left = "2";
+        pape.Final_pos.Z_left = "3";
+
+        pape.Initial_ori = new Orientation();
+        pape.Initial_ori.Yaw_left = "1";
+        pape.Initial_ori.Pitch_left = "1";
+        pape.Initial_ori.Roll_left = "1";
+
+        pape.Final_ori = new Orientation();
+        pape.Final_ori.Yaw_left = "1";
+        pape.Final_ori.Pitch_left = "1";
+        pape.Final_ori.Roll_left = "1";
+
+        pape.Speed = new Speed();
+        pape.Speed.Speed_left = "0.2";
+
+        pape.Angls = new Angles();
+        pape.Mode = Reference_sys.ABSOLUTE;
+        pape.Kin = Kinematics.INVERSE;
+
+        return pape;
+    }
+
+    void OnApplicationClose()
+    {
+        thread.Abort();
+    }
+}
