b33db4cb54322c00236a198e440f415920f44427
Fabian Spaniol <s9faspan@stud.uni-saarland.de>
2016-08-31 16:28:56 +0200

    Vuforia right push

---
+++ Vuforia Test/Assets/Scripts/PublishObjPosRot.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+using Vuforia;
+
+public class PublishObjPosRot : MonoBehaviour {
+
+    PublishPosRot.Publisher publisher;
+    Thread publishThread;
+    public de.dfki.events.Device device;
+    public String serverAddr = "localhost";
+    public int serverPort = 9000;
+    public int id { get; set; }
+
+    void Start()
+    {
+        System.Random rnd = new System.Random();
+        id = rnd.Next();
+        publisher = new PublishPosRot.Publisher{
+            device = this.device,
+            serverAddr = this.serverAddr,
+            serverPort = this.serverPort,
+            id = this.id
+        };
+        publishThread = new Thread(publisher.Connect);
+        publishThread.Start();
+    }
+
+
+    void FixedUpdate()
+    {
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            if (tb.gameObject == gameObject)
+            {
+                publisher.SendPosition(gameObject);
+                publisher.SendRotation(gameObject);
+            }
+        }
+    }
+
+
+    void OnApplicationQuit()
+    {
+        publisher.close();
+        publishThread.Abort();
+    }
+}
