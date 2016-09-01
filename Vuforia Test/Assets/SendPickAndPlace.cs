2835f9e9869fe5bd098406b9c157367dd9ba16a3
Fabian Spaniol <s9faspan@stud.uni-saarland.de>
2016-09-01 21:51:12 +0200

    BaxterMovement with Mouse added: Not Tested, not finished

--- Vuforia Test/Assets/SendPickAndPlace.cs
+++ Vuforia Test/Assets/SendPickAndPlace.cs
@@ -10,10 +10,18 @@ public class SendPickAndPlace : MonoBehaviour
     private PickAndPlace pick_n_place;
     private Thread thread;
 
-	// Use this for initialization
-	void Start()
+    public bool pickandPlaceButtonClicked;
+    private int state;
+    private GameObject movefrom;
+    private Vector3 moveto;
+
+    public Vector3 armAtFixedMarkerposition;
+    public Vector3 armAtFixedMarkerrotation;
+    public GameObject fixedMarker;
+    // Use this for initialization
+    void Start()
     {
-        pick_n_place = new PickAndPlace
+        /*pick_n_place = new PickAndPlace
         {
             serverAddr = "localhost",
             serverPort = 9000,
@@ -61,7 +69,128 @@ public class SendPickAndPlace : MonoBehaviour
         };
 
         thread = new Thread( pick_n_place.Send );
-        thread.Start();
+        thread.Start();*/
+        state = 0;
+        pickandPlaceButtonClicked = false;
+    }
+
+    private void SendPAP(Vector3 from, Vector3 to)
+    {
+        if (true/*!thread.IsAlive*/)
+        {
+            Vector3 pickpos = from - fixedMarker.transform.position + armAtFixedMarkerposition;
+            Vector3 placepos = to - fixedMarker.transform.position + armAtFixedMarkerposition;
+            //Rotation currently only Armposition of Baxter when picking one Object. Is this even always the same for same Objectrotation???
+            Vector3 pickrot = armAtFixedMarkerrotation;
+            pickrot = pickrot * Mathf.PI / 180;
+            Vector3 placerot = armAtFixedMarkerrotation;
+            placerot = placerot * Mathf.PI / 180;
+            Debug.Log(placerot);
+            pick_n_place = new PickAndPlace
+            {
+                serverAddr = "localhost",
+                serverPort = 9000,
+                pap_event = new PickAndPlaceEvent
+                {
+                    Limb = Limb.LEFT,
+
+                    Initial_pos = new Position
+                    {
+                        X_left = pickpos.x.ToString(),
+                        Y_left = pickpos.y.ToString(),
+                        Z_left = pickpos.z.ToString()
+                    },
+
+                    Final_pos = new Position
+                    {
+                        X_left = placepos.x.ToString(),
+                        Y_left = placepos.y.ToString(),
+                        Z_left = placepos.z.ToString()
+                    },
+
+                    Initial_ori = new Orientation
+                    {
+                        Yaw_left = pickrot.x.ToString(),
+                        Pitch_left = pickrot.y.ToString(),
+                        Roll_left = pickrot.z.ToString()
+                    },
+
+                    Final_ori = new Orientation
+                    {
+                        Yaw_left = placerot.x.ToString(),
+                        Pitch_left = placerot.y.ToString(),
+                        Roll_left = placerot.z.ToString()
+                    },
+
+                    Speed = new Speed
+                    {
+                        Speed_left = "0.2"
+                    },
+
+                    Angls = new Angles(),
+                    Mode = Reference_sys.ABSOLUTE,
+                    Kin = Kinematics.INVERSE
+                }
+            };
+            thread = new Thread(pick_n_place.Send);
+            thread.Start();
+        }
+        else
+        {
+            Debug.Log("Pick and Place not Send, old thread still running");
+        }
+        state = 0;
+        pickandPlaceButtonClicked = false;
+    }
+
+    public void ClickPAPButton()
+    {
+        pickandPlaceButtonClicked = !pickandPlaceButtonClicked;
+        state = 0;
+    }
+
+    void Update()
+    {
+        if (pickandPlaceButtonClicked)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Test");
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider != null)
+                    {
+                        if (state == 0)
+                        {
+                            if (hit.collider.GetType() == typeof(MeshCollider))
+                            {
+                                Debug.Log("No valid object");
+                            }
+                            else
+                            {
+                                movefrom = hit.collider.gameObject;
+                                Debug.Log("GameObject at " + movefrom.transform.position + " selected");
+                                state++;
+                            }
+                        }
+                        else
+                        {
+                            moveto = hit.point;
+                            Debug.Log("Move" + movefrom.transform.position + "to" + moveto);
+                            SendPAP(movefrom.transform.position, moveto);
+                            pickandPlaceButtonClicked = false;
+                            state = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log("Not clicked in valid area");
+                }
+            }
+        }
     }
 
     void OnApplicationClose()
