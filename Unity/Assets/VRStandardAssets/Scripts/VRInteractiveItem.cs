70adf6186db1fa99d43b1ef5692347d8c7100e8c
felix.scherzinger <felix.scherzinger@gmx.de>
2016-09-07 11:38:29 +0200

    drag and drop on GearVR implemented

--- Unity/Assets/VRStandardAssets/Scripts/VRInteractiveItem.cs
+++ Unity/Assets/VRStandardAssets/Scripts/VRInteractiveItem.cs
@@ -16,7 +16,6 @@ namespace VRStandardAssets.Utils
         public event Action OnUp;               // Called when Fire1 is released whilst the gaze is over this object.
         public event Action OnDown;             // Called when Fire1 is pressed whilst the gaze is over this object.
 
-
         protected bool m_IsOver;
 
 
