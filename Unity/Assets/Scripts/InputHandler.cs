531404697a48786d99c48a9a5d27789469e9c9e4
felix.scherzinger <felix.scherzinger@gmx.de>
2016-09-07 14:46:36 +0200

    Revert to 378a3620cc7261812f12573ef3a862e028414a34

--- Unity/Assets/Scripts/InputHandler.cs
+++ Unity/Assets/Scripts/InputHandler.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using VRStandardAssets.Utils;
 
 // This script is a simple example of how an interactive item can
 // be used to change things on gameobjects by handling events.
 public class InputHandler : MonoBehaviour
 {
+	[SerializeField] private Image reticle;
+	[SerializeField] private VRInput m_VRInput;
 	[SerializeField] private Material m_NormalMaterial;                
 	[SerializeField] private Material m_OverMaterial;                  
 	[SerializeField] private Material m_ClickedMaterial;               
 	[SerializeField] private Material m_DoubleClickedMaterial;         
 	[SerializeField] private VRInteractiveItem m_InteractiveItem;
 	[SerializeField] private Renderer m_Renderer;
-
+	private bool draggable = false;
 
 	private void Awake ()
 	{
@@ -25,6 +28,7 @@ public class InputHandler : MonoBehaviour
 		m_InteractiveItem.OnOut += HandleOut;
 		m_InteractiveItem.OnClick += HandleClick;
 		m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+		m_VRInput.OnClick += HandleSecondClick;
 	}
 
 
@@ -58,6 +62,9 @@ public class InputHandler : MonoBehaviour
 	{
 		Debug.Log("Show click state");
 		m_Renderer.material = m_ClickedMaterial;
+		draggable = true;
+		Debug.Log(draggable);
+		m_InteractiveItem.gameObject.GetComponent<BoxCollider>().enabled = false;
 	}
 
 
@@ -67,4 +74,23 @@ public class InputHandler : MonoBehaviour
 		Debug.Log("Show double click");
 		m_Renderer.material = m_DoubleClickedMaterial;
 	}
+
+	public void Update(){
+		if(draggable){
+			Vector3 pos = reticle.transform.position;
+			pos.y += .1f;
+			m_InteractiveItem.transform.position = pos;
+		}
+	}
+
+	public void HandleSecondClick(){
+		Debug.Log("Second click");
+		if(draggable){
+			draggable = false;
+			m_InteractiveItem.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			m_InteractiveItem.gameObject.GetComponent<BoxCollider>().enabled= true;
+			m_InteractiveItem.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		}
+	}
+		
 }
