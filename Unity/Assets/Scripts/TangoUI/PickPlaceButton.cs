b3bd5e5634f051e5d3d4c97b5e06612757dcb318
Andrea vergessen <Andrea vergessen>
2016-09-14 16:49:39 +0200

    added scripts to buttons

---
+++ Unity/Assets/Scripts/TangoUI/PickPlaceButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using de.dfki.events;
+
+public class PickPlaceButton : MonoBehaviour {
+
+	public void PickandPlace(GameObject go)
+    {
+        Vector3 position = transform.position;
+        PositionEvent e = new PositionEvent(Device.TANGO, ObjType.CUBE, new Position(position.x, position.y, position.z), go.GetComponent<MetaData>().id);
+        RPCClient.client.Move(e);
+    }
+}
