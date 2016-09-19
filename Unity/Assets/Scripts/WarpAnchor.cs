7f421006f396062f8d583801ce64a51feb24330e
felix.scherzinger <felix.scherzinger@gmx.de>
2016-09-19 16:22:02 +0200

    added warp ranges

--- Unity/Assets/Scripts/WarpAnchor.cs
+++ Unity/Assets/Scripts/WarpAnchor.cs
@@ -7,6 +7,11 @@ public class WarpAnchor : MonoBehaviour {
 
 	[SerializeField] private VRInput vRInput;
 	[SerializeField] private Image reticle;
+	const float max_z = 0.4f;
+	const float min_z = -2.4f;
+	const float max_x = 2.6f;
+	const float min_x = -0.2f;
+
 
 	private void OnEnable()
 	{
@@ -23,6 +28,14 @@ public class WarpAnchor : MonoBehaviour {
 		if(GearVRMenu.currentTool == GearVRMenu.Tool.WARP){
 			Vector3 new_pos = reticle.transform.position;
 			Vector3 curr_pos = this.gameObject.transform.position;
+			if(new_pos.x < min_x)
+				new_pos.x = min_x;
+			if(new_pos.x > max_x)
+				new_pos.x = max_x;
+			if(new_pos.z < min_z)
+				new_pos.z = min_z;
+			if(new_pos.z > max_z)
+				new_pos.z = max_z;
 			this.gameObject.transform.position = new Vector3(new_pos.x, curr_pos.y, new_pos.z);
 		}
 	}
