bff200d6329ad0bde5a8b1a841a930fa8563ee84
felix.scherzinger <felix.scherzinger@gmx.de>
2016-09-20 11:51:51 +0200

    new tango project

---
+++ NewTango1.0/Assets/Scripts/ReceivedObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Threading;
+
+using de.dfki.tecs;
+using de.dfki.tecs.ps;
+using de.dfki.tecs.basetypes;
+using System.Runtime.InteropServices;
+
+using de.dfki.events;
+using System.Collections.Generic;
+//using VRStandardAssets.Utils;
+using UnityEngine.UI;
+
+public class ReceivedObject : MonoBehaviour {
+
+
+    void Start()
+	{
+	}
+
+
+	void Update(){
+	}
+
+	public void updatePosition(PositionEvent pe ){
+			Vector3 newpos = new Vector3((float)pe.Position.X,(float)pe.Position.Y,(float)pe.Position.Z);
+			if ( (transform.position - newpos).sqrMagnitude > 0.0025)
+				transform.position = newpos;
+	}
+		
+	public void updateDirection(DirectionEvent de){
+			Quaternion newrot = new Quaternion((float)de.Direction.X, (float)de.Direction.Y, (float)de.Direction.Z, (float)de.Direction.W);
+		if ( Quaternion.Angle(transform.rotation,newrot) > 1)
+				transform.rotation = newrot;
+	}
+		
+}
+  
---
+++ NewTango1.0/Assets/Scripts/TangoUI/PointButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointButton : MonoBehaviour {
+
+    public ButtonHandler Handler;
+
+    public void point()
+    {
+        Handler.setCurrent(ButtonHandler.SelectedButton.point);
+    }
+}
