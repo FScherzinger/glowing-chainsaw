de6ae56f36f605350ca12c4c5d037858e7b57ea2
felix.scherzinger <felix.scherzinger@gmx.de>
2016-09-21 09:28:02 +0200

    updated tangoAR scene

---
+++ Unity/Assets/CameraAttachement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAttachement : MonoBehaviour {
+
+	[SerializeField] public GameObject ARCam;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = ARCam.transform.position;
+	}
+}
