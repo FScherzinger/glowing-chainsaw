f23c08a492e1e63e92442c6b7209302f9f586841
Thorsten Schamper <thorsten.schamper@gmail.com>
2016-09-21 13:32:12 +0200

    changed hardcoded id to a random one

--- Unity/Assets/Scripts/PublishCam.cs
+++ Unity/Assets/Scripts/PublishCam.cs
@@ -22,7 +22,7 @@ public class PublishCam : MonoBehaviour {
 		pubThread = new Thread(pub.Connect);
 		pubThread.Start ();
 
-        camID = 5;// RPCClient.client.getUniqueCameraId ();
+        camID = new System.Random().Next();// RPCClient.client.getUniqueCameraId ();
 		publish_pos_rot = PublishPosRot (.2f);
 		StartCoroutine (publish_pos_rot);
 	}
