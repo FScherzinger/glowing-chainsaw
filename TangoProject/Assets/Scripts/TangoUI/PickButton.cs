5b6dec5ece700e8656cb9e65295823a6b3beb17e
Andrea vergessen <Andrea vergessen>
2016-09-27 12:01:05 +0200

    added Buttons to new TangoProject

---
+++ TangoProject/Assets/Scripts/TangoUI/PickButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickButton : MonoBehaviour {
+
+    [SerializeField] private ButtonHandler Handler;
+    [SerializeField] private MenuButton Menu;
+
+
+    public void pick()
+    {
+        Debug.Log("picking mode");
+        Handler.setCurrent(ButtonHandler.SelectedButton.pickAndPlace);
+        Menu.CollapseMenu();
+    }
+}
